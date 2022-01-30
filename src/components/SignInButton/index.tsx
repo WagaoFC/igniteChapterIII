2c558821acda426f34cfd103892cfbf893d3d21a
WagaoFC <wagnerferreira_07@hotmail.com>
2022-01-29 23:44:32 -0300

    finalizando autenticacao com session e signOut

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,6 +1,6 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
-import { signIn, useSession } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 import styles from './styles.module.scss'
 
 export function SignInButton() {
@@ -10,9 +10,10 @@ export function SignInButton() {
         <button 
             type="button"
             className={styles.signInButton}
+            onClick={() => signOut()}
         >
             <FaGithub color='#04d361' />
-            Wagner Ferreira Costa
+            {session.user.name}
             <FiX color='#737380' className={styles.closeIcon} />
         </button>
     ) : 
