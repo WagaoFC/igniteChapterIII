318724a9c06076da03880bff718ee8af93f54c91
WagaoFC <wagnerferreira_07@hotmail.com>
2022-01-31 21:54:27 -0300

    salvando usuario no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from 'faunadb'
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -8,9 +10,27 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       authorization: {
         params: {
-          scope: 'read:user',
+          scope: 'read:user, user:email',
         },
       },
     }),
   ],
+  callbacks: {
+    async signIn({user, account, profile}) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+          
+        return true
+      } catch {
+        return false
+      }
+    },
+  }
 })
